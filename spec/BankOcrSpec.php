a775338e84e5c18e5c3ad0a6430d9f8d90478018
alexesba <alexesba@gmail.com>
2014-07-28 23:12:15 -0500

    added specs to validate the file exists, lenght for account_number and digit number

--- spec/BankOcrSpec.php
+++ spec/BankOcrSpec.php
@@ -7,6 +7,7 @@ use Prophecy\Argument;
 
 class BankOcrSpec extends ObjectBehavior
 {
+
     function it_is_initializable()
     {
         $this->shouldHaveType('BankOcr');
@@ -16,4 +17,21 @@ class BankOcrSpec extends ObjectBehavior
       $this->load('file');
     }
 
+    function it_throw_an_error_when_reads_the_file_if_not_exists()
+    {
+      $this->load('fake');
+      $this->shouldThrow('Exceptions\FileNotFound')->duringRead();
+    }
+
+    function it_parse_the_file_content_reading_lines(){
+      $this->load('file');
+      $this->getNextLine();
+    }
+
+    function it_should_has_length_account_with_exact_nine_digits(){
+      $this->length_account->shouldbeEqualTo(9);
+    }
+    function it_should_has_digit_length_with_exact_three_digits(){
+      $this->digit_length->shouldbeEqualTo(3);
+    }
 }
--- src/BankOcr.php
+++ src/BankOcr.php
@@ -1,12 +1,48 @@
 <?php
 
+use Exceptions\FileNotFound;
 class BankOcr
 {
+  var $length_account = 9;
+  var $digit_length   = 3;
+
+  protected $lines     = array();
 
 
+  public function load($file_path)
+  {
+    $this->file = $file_path;
+    $this->handle = NULL;
+  }
 
-    public function load($file_path)
+  public function read()
+  {
+    if(!file_exists($this->file))
     {
-        $this->file = $file_path;
+      throw new FileNotFound('The does not exist!.');
     }
+    $this->handle = @fopen($this->file, 'r');
+
+    return $this;
+  }
+
+  public function setLine($line){
+    $this->lines[] = $line;
+  }
+
+
+  public function getNextLine()
+  {
+    if($this->handle){
+      if(($current_line = fgets($this->handle, 4096)) !== false){
+        $this->setLine($current_line);
+      }
+    }
+  }
+
+  public function linesToNumbers(){
+    echo $this->lines[0][4];
+  }
+
+
 }
