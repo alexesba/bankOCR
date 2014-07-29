7a2e14b59330d7145b26319ea26cfddf041710bd
alexesba <alexesba@gmail.com>
2014-07-28 20:17:28 -0500

    added BankOcr class

---
+++ spec/BankOcrSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class BankOcrSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('BankOcr');
+    }
+
+}
