9c5dc59a397a969fa6df2c31f5eb44fc786f33b3
alexesba <alexesba@gmail.com>
2014-07-30 04:38:32 -0500

    adde method to load the file with sample data, added test cases for each of the numbers, 0 to 9, remove end of each line before store the content in the array. added method to get a unique identifier for each pipe number

--- spec/BankOcrSpec.php
+++ spec/BankOcrSpec.php
@@ -7,14 +7,20 @@ use Prophecy\Argument;
 
 class BankOcrSpec extends ObjectBehavior
 {
+    const BASEFILE = __DIR__;
+
+    static function file_path($file){
+      return self::BASEFILE."/../src/$file";
+    }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('BankOcr');
     }
 
-    function it_load_a_file(){
-      $this->load('file');
+    function it_loads_a_file(){
+      $file = '/Users/pair/work/BanckOCR/src/file';
+      $this->load($file)->file->shouldbeEqualTo($file);
     }
 
     function it_throw_an_error_when_reads_the_file_if_not_exists()
@@ -23,15 +29,94 @@ class BankOcrSpec extends ObjectBehavior
       $this->shouldThrow('Exceptions\FileNotFound')->duringRead();
     }
 
-    function it_parse_the_file_content_reading_lines(){
-      $this->load('file');
-      $this->getNextLine();
+    function it_parse_the_file_content_into_lines(){
+      $file = self::file_path('file');
+      $this->load($file)->read();
+      $this->getNextLine()->lines->beAnInstanceOf('array');
+      $this->lines->shouldHaveCount(4);
     }
 
     function it_should_has_length_account_with_exact_nine_digits(){
       $this->length_account->shouldbeEqualTo(9);
     }
+
     function it_should_has_digit_length_with_exact_three_digits(){
       $this->digit_length->shouldbeEqualTo(3);
+    }
+
+
+    function it_process_the_file_with_cero_numbers(){
+      $file = self::file_path('0.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("000000000");
+
+    }
+    function it_process_the_file_with_numbers_one(){
+      $file = self::file_path('1.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("111111111");
+
+    }
+
+    function it_process_the_file_with_numbers_two(){
+      $file = self::file_path('2.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("222222222");
+
+    }
+
+    function it_process_the_file_with_numbers_three(){
+      $file = self::file_path('3.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("333333333");
+    }
+
+    function it_process_the_file_with_numbers_four(){
+      $file = self::file_path('4.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("444444444");
+    }
+
+    function it_process_the_file_with_numbers_five(){
+      $file = self::file_path('5.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("555555555");
+    }
+
+    function it_process_the_file_with_numbers_six(){
+      $file = self::file_path('6.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("666666666");
+    }
+
+    function it_process_the_file_with_numbers_seven(){
+      $file = self::file_path('7.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("777777777");
+    }
+
+    function it_process_the_file_with_numbers_eight(){
+      $file = self::file_path('8.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("888888888");
+    }
+    function it_process_the_file_with_numbers_nine(){
+      $file = self::file_path('9.txt');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->account_number->shouldbeEqualTo("999999999");
+    }
+
+    function it_process_the_file_content_into_numbers(){
+      $file = self::file_path('file');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->getAccountNumber()->shouldbeEqualTo("123456789");
+    }
+
+    function case_use_3()
+    {
+      $file = self::file_path('case3');
+      $this->load($file)->read()->getNextLine();
+      $this->linesToNumbers()->getAccountNumber()->shouldbeEqualTo("000000051");
+
     }
 }
--- src/BankOcr.php
+++ src/BankOcr.php
@@ -5,21 +5,25 @@ class BankOcr
 {
   var $length_account = 9;
   var $digit_length   = 3;
+  var $numbers        = array();
+  var  $flag = "";
 
-  protected $lines     = array();
+  var $lines     = array();
+
 
 
   public function load($file_path)
   {
     $this->file = $file_path;
     $this->handle = NULL;
+    return $this;
   }
 
   public function read()
   {
     if(!file_exists($this->file))
     {
-      throw new FileNotFound('The does not exist!.');
+      throw new FileNotFound('The file does not exist!.');
     }
     $this->handle = @fopen($this->file, 'r');
 
@@ -33,16 +37,112 @@ class BankOcr
 
   public function getNextLine()
   {
+    #read recursively the file
     if($this->handle){
-      if(($current_line = fgets($this->handle, 4096)) !== false){
-        $this->setLine($current_line);
+      if(($current_line = fgets($this->handle, 1024)) !== false){
+        $this->setLine(preg_replace('/\r\n|\r|\n/', ' ', $current_line));
+        $this->getNextLine();
       }
     }
+    return $this;
   }
 
   public function linesToNumbers(){
-    echo $this->lines[0][4];
+    $this->processLines();
+    return $this;
+  }
+
+  public function getAccountNumber(){
+    return $this->account_number.$this->flag;
+  }
+
+  public function processLines(){
+    for( $index=0; $index < $this->length_account; $index++ ){
+      $this->numbers[] = $this->stringToNumber($this->getSubstring($index));
+    }
+
+    $this->account_number = implode($this->numbers, '');
+
+    if(!$this->isValid()){
+      if(in_array('?', $this->numbers, true)){
+        $this->flag.=" ERR";
+      }else{
+        $this->flag.=" ILL";
+      }
+
+    }
+    return $this;
+  }
+
+  public function isValid(){
+    return ($this->checkSum() %  11) === 0;
+  }
+
+  public function checkSum(){
+    foreach(array_reverse($this->numbers) as $index=>$value ){
+      if($index == 0)
+      {
+        $result = (integer)$value;
+      }else{
+        $index++;
+        $result+= ($index * $value);
+      }
+    }
+    return $result;
+  }
+
+  public function getSubstring($index)
+  {
+    $start = ($index * $this->digit_length);
+    $a = substr($this->lines[0], $start, $this->digit_length );
+    $b = substr($this->lines[1], $start, $this->digit_length );
+    $c = substr($this->lines[2], $start, $this->digit_length );
+    $d = substr($this->lines[3], $start, $this->digit_length );
+    return "$a$b$c$d";
+  }
+
+  public function stringToNumber($string){
+
+    switch(md5($string))
+    {
+      case '49aba1ce5f1d22713f7be4d15cff8bae':
+        return 0;
+        break;
+
+      case '805d68b92dafd5181c807665831d70fc':
+        return 1;
+        break;
+      case '698d411a09d832519c7a91fbf8bfe7e6':
+        return 2;
+        break;
+      case '215b524980706407d560fa28a5aee7fb':
+        return 3;
+        break;
+      case '6996d3a504fde8c96b837d99507c04ee':
+        return 4;
+        break;
+      case 'd1b1bc32561993df52a37db677598f6a':
+        return 5;
+        break;
+      case '5cda28cf7f9302c0f0fe5682454bae81':
+        return 6;
+        break;
+      case 'cac39771f632cc83bd60c9b48816f153':
+        return 7;
+        break;
+      case '8bd184d9e57f27a66f10904dd13884df':
+        return 8;
+        break;
+      case 'be5f4894297e0e487df9a213c3f989ed':
+        return 9;
+        break;
+      default:
+        return '?';
+        break;
+
+    }
   }
 
 
 }
+
